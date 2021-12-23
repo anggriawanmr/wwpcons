a011c01e85b2ea1043c2a7f5ec82800f736bef07
anggriawanmr <anggriawanmr@gmail.com>
2021-12-23 17:13:56 +0700

    Adding palette to the banner :lipstick:

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -2,48 +2,118 @@
 import { jsx } from 'theme-ui';
 import { Container, Box, Heading, Text, Image, Button, Link } from "theme-ui";
 import { keyframes } from "@emotion/core";
-import BannerImg from "assets/banner-thumb.jpg";
 import { IoIosArrowDown } from "react-icons/io";
-import { useEffect } from "react";
+import bannerVideo from "assets/bannerVideo.gif";
 
 export default function Banner() {
-  // const scroll = (e) => {
-  //   e.preventDefault();
-  //   window.scrollTo({
-  //     top: 700,
-  //     behavior: "smooth",
-  //   });
-  // };
+  const texts = [
+    "Transparent",
+    "Collaborative",
+    "Nurturing",
+    "Autonomous",
+    "Nimble",
+    "Progressive",
+    "Flexible",
+    "Innovative",
+  ]; // from observable hq web site
 
-  // const scrollTo = useEffect(() => {
-  //   window.addEventListener("click", scroll);
+  const Texts = (props) => {
+    const [state, setState] = React.useState({
+      left: "",
+      right: "",
+      texts: props.texts,
+      current: props.texts[0],
+    });
+    const stay = React.useRef(false);
+    const staybt = React.useRef(false);
+    const update = React.useCallback(() => {
+      const addNextChar = () => {
+        setState({
+          ...state,
+          left: state.left + state.texts[0].slice(0, 1),
+          right: state.texts[0].slice(1),
+          texts: state.texts.map((e, i) => (i === 0 ? e.slice(1) : e)),
+        });
+      };
+      const deleteLastChar = () => {
+        if (state.left.slice(0, -1) === "") {
+          stay.current = true;
+          staybt.current = true;
+        }
+        setState({
+          ...state,
+          left: state.left.slice(0, -1),
+          right: state.left.slice(-1) + state.right,
+        });
+      };
+      const switchToNextText = () => {
+        stay.current = false;
+        staybt.current = false;
+        const nextText = state.texts[1];
+        setState({
+          ...state,
+          left: nextText.slice(0, 1), // first char of next text
+          right: nextText.slice(1), // rest of the next char
+          texts: [...state.texts.slice(1).map((e, i) => (i === 0 ? e.slice(1) : e)), state.current],
+          current: nextText,
+        });
+      };
+      if (state.texts[0].length === 0) {
+        stay.current = false;
+        if (state.left === "") switchToNextText();
+        else deleteLastChar();
+      } else addNextChar();
+    }, [stay, staybt, setState, state]);
+    React.useEffect(() => {
+      setTimeout(
+        () => {
+          if (state.texts[0].length === 1) stay.current = true;
+          window.requestAnimationFrame(update);
+        },
+        stay.current
+          ? staybt.current
+            ? props.waitbt || 30
+            : props.wait || 3000
+          : props.speed || 30
+      );
+    }, [update]);
+    return (
+      <span className={props.className}>
+        {state.left}
+        <span>{state.right}</span>
+      </span>
+    );
+  };
 
-  //   return () => {
-  //     window.removeEventListener("click", scroll);
-  //   };
-  // }, []);
+  const scrolling = () => {
+    window.scrollTo({
+      top: 700,
+      behavior: "smooth",
+    });
+  };
+
+  _onMouseMove = () => {};
 
   return (
-    <section sx={styles.banner} id="home">
+    <section sx={styles.banner} id="home" onMouseMove={this._onMouseMove}>
       <Container sx={styles.banner.container}>
         <Box sx={styles.banner.contentBox}>
           <Heading as="h1" variant="heroPrimary">
-            Best Waste Water Treatment Plant Company
+            The Most <Texts sx={styles.texts} waitbt={50} wait={2000} speed={27} texts={texts} />{" "}
+            Waste Water Treatment Company
           </Heading>
+
           <Text as="p" variant="heroSecondary">
             This red is too red thats not what i saw in my head at all nor thats not what i saw in
             my head at all the target audience is makes and famles aged zero and up
           </Text>
-          <Button variant="primary" sx={styles.button}>
-            Explore
-          </Button>
         </Box>
 
         <Box sx={styles.banner.imageBox}>
-          <Image src={BannerImg} alt="banner" />
+          <Image src={bannerVideo} alt="banner" />
         </Box>
       </Container>
-      <Box sx={styles.scroll}>
+      <Box sx={styles.scroll} onClick={scrolling}>
         <Text sx={styles.scrollText}>What's Next</Text>
         <IoIosArrowDown sx={styles.scrollButton} />
       </Box>
@@ -69,7 +139,7 @@ const styles = {
     pb: [2, null, 0, null, 2, 0, null, 5],
     position: "relative",
     height: "100vh",
-    backgroundColor: "#000",
+    backgroundColor: "#A3423C",
     backgroundSize: "cover",
     zIndex: 2,
     "&::before": {
@@ -109,26 +179,30 @@ const styles = {
       mx: "auto",
       textAlign: "left",
       mb: ["40px", null, null, null, null, 7],
+      zIndex: 4,
     },
     imageBox: {
       justifyContent: "center",
       textAlign: "center",
       display: "inline-flex",
+      top: 30,
       width: "50%",
       height: "50%",
-      mb: [0, null, -6, null, null, "-40px", null, -3],
+      mb: [0, null, -6, null, null, "-10px", null, -3],
       img: {
-        position: "relative",
-        height: [245, "auto"],
+        position: "absolute",
+        height: [100, 450],
+        top: "15%",
+        zIndex: 1,
       },
     },
-    button: {
-      ":hover": {
-        transform: "translateY(-5px)",
-      },
-      ":active": {
-        transform: "translateY(3px)",
-      },
+  },
+  texts: {
+    opacity: 1,
+    color: "#DE834D",
+    "& > span": {
+      opacity: 0.2,
+      color: "palevioletred",
     },
   },
   scroll: {
@@ -137,7 +211,7 @@ const styles = {
     top: "95%",
     left: "50%",
     transform: "translate(-50%, -95%)",
-    color: "#fff",
+    color: "#F0D290",
     textAlign: "center",
     fontSize: "30px",
   },
